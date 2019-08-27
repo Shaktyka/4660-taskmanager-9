58c42f8e078ecffa853a25e2c6c9bdfc0df804db
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-08-27 22:35:01 +0600

    Добавляет обработчик на нажатие Esc, когда открыта карточка редакт-ния

--- src/main.js
+++ src/main.js
@@ -138,27 +138,42 @@ const onLoadMoreBtnClick = (evt) => {
   // Добавление в контейнер ещё карточек из массива
 };
 
-// Замена одного элемента на другой
-
-
-// Рендеринг ещё 7 тасков
+// Рендеринг карточек задач
 const renderTasks = (container, tasksArr) => {
   let fragment = new DocumentFragment();
   tasksArr.forEach((taskEl) => {
 
     const task = new Task(taskEl).getElement();
-    const editTask = new EditTask(taskEl).getElement();
-
     const editBtn = task.querySelector(`.card__btn--edit`);
+    const editTask = new EditTask(taskEl).getElement();
+    const editForm = editTask.querySelector(`form`);
+    const textarea = editTask.querySelector(`textarea`);
+
+    const documentEscKeydownHandler = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        container.replaceChild(task, editTask);
+        document.removeEventListener(`keydown`, documentEscKeydownHandler);
+      }
+    };
+
+    textarea.addEventListener(`focus`, () => {
+      document.removeEventListener(`keydown`, documentEscKeydownHandler);
+    });
+
+    textarea.addEventListener(`blur`, () => {
+      document.addEventListener(`keydown`, documentEscKeydownHandler);
+    });
+
     editBtn.addEventListener(`click`, (evt) => {
       evt.preventDefault();
       container.replaceChild(editTask, task);
+      document.addEventListener(`keydown`, documentEscKeydownHandler);
     });
 
-    const editForm = editTask.querySelector(`form`);
     editForm.addEventListener(`submit`, (evt) => {
       evt.preventDefault();
       container.replaceChild(task, editTask);
+      document.removeEventListener(`keydown`, documentEscKeydownHandler);
     });
 
     fragment.appendChild(task);
@@ -185,10 +200,7 @@ const renderStartContent = (container, tasksArr) => {
     render(contentContainer, new TasksContainer().getElement());
     const tasksContainer = document.querySelector(`.board__tasks`);
 
-    // Рендерим карточку Edit
-    // render(tasksContainer, new EditTask(tasksArray[0]).getElement());
     // Рендерим остальные карточки
-    // render(tasksContainer, createElement(makeTask(makeTaskData())));
     renderTasks(tasksContainer, tasksArray.slice(0, TasksAmount.START));
 
     // Рендерим кнопку "LoadMore"
